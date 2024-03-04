2b43c3c541e799cb25fb55c9b370df4af70d4f2e
imranio07 <107305213+Imranio07@users.noreply.github.com>
2024-03-04 22:22:02 +0600

    Add files via upload
    ypython

---
+++ ytpython3.py
@@ -0,0 +1,8 @@
+##integer = input("Enter a number: ")
+##if integer.isdigit():
+##    integer = int(integer)
+##print(type(integer))
+
+name = 'Alice'
+age = '25'
+print(f"Hello, my name is {name} and I'm {age} years old")
